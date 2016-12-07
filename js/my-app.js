8c2dc141a9d4538a3d477b46d3e291632d08326f
sagary09 <sagaryukta@gmail.com>
2016-12-07 11:38:48 -0500

    swiper

--- js/my-app.js
+++ js/my-app.js
@@ -13,8 +13,11 @@ var mainView = myApp.addView('.view-main', {
 
 var mySwiper = myApp.swiper('.swiper-container', {
     pagination:'.swiper-pagination',
-    runCallbacksOnInit:true
   });
+
+var mySwiper = new Swiper ('.swiper-container', {
+	autoHeight: true,
+});
 // Callbacks to run specific code for specific pages, for example for About page:
 myApp.onPageInit('all_events', function (page) {
     // run createContentPage func after link was clicked
@@ -93,6 +96,8 @@ function showDiv3(){
   div.style.display = 'block';
   div= document.getElementById('txt3');
   div.style.display = 'block';
+  div= document.getElementById('button_up');
+  div.style.display = 'block';
   div=document.getElementById('check-deselect3');
   div.style.display = 'none';
 }
@@ -107,11 +112,15 @@ function showDiv4(){
 function showDiv5(){
   div= document.getElementById('check5');
   div.style.display = 'block';
+  div= document.getElementById('txt5');
+  div.style.display = 'block';
   div=document.getElementById('check-deselect5');
   div.style.display = 'none';
 }
 function showDiv6(){
   div= document.getElementById('check6');
+  div.style.display = 'block';
+  div= document.getElementById('txt6');
   div.style.display = 'block';
   div=document.getElementById('check-deselect6');
   div.style.display = 'none';
@@ -119,9 +128,20 @@ function showDiv6(){
 function showDiv7(){
   div= document.getElementById('check7');
   div.style.display = 'block';
+  div= document.getElementById('txt7');
+  div.style.display = 'block';
   div=document.getElementById('check-deselect7');
   div.style.display = 'none';
 }
+function showDiv8(){
+  div= document.getElementById('check8');
+  div.style.display = 'block';
+  div= document.getElementById('txt8');
+  div.style.display = 'block';
+  div=document.getElementById('check-deselect8');
+  div.style.display = 'none';
+}
+
 function myFunction(){
     console.log('hello');
     $$('.page-content').scrollTop($$('#section2').offset().top, 300);
@@ -138,10 +158,12 @@ function up2(){
     console.log('test3');
     $$('.page-content').scrollTop(670, 300);
 }
-function onSlideChangeEnd(swiper){
-    console.log('Narrative');
-      $$('.page-content').scrollTop($$('#swiper-slide-narrative').offset().top, 300);
+function up4(){
+    console.log('test4');
+    $$('.page-content').scrollTop(0, 300);
 }
+
+
 
 // function click1(){
 //   document.all.section2.scrollIntoView(true);
@@ -171,6 +193,7 @@ myApp.onPageInit('empty_screens', function (page) {
             render(results);
         }
     });
+
 });
 
 myApp.onPageInit('event-step1', function (page) {
